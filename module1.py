67d0d522c6f7aa9da7edc01466c02731f7eb3ffe
Sergei <mailbox.sa02@gmail.com>
2022-05-18 16:48:07 +0200

    Finished main loop

--- module1.py
+++ module1.py
@@ -1,22 +1,179 @@
-from soupsieve import select
-from module2 import *
-from module2 import select_survivors
+from random import randint
+from itertools import product
+from module2 import EA
+
+GENE_SIZE = 195
+
+num_gen = 51
+pop_size = 100
+cross_prob = 0.1
+mut_prob = 0.1
 
 
-def init(pop_size, gen_num, cross_prob, mut_prob):
-    x_gen = generate_init_pop(pop_size)  # generation 0
-    f_gen = evaluate(x_gen)
+def main(pop_size, num_gen, cross_prob, mut_prob):
+    # initialize the population
+    x_gen = generatePopulation(pop_size)  # create generation 0
+    f_gen = evaluate(x_gen)  # get the fitness value of generation 0
 
-    for gen in gen_num:
-        x_par = select_parents(x_gen)
-        x_new_par = crossover(x_par)
-        x_child = mutation(x_new_par)
-        x_next_gen = select_survivors(x_child)
-        f_net_gen = evaluate(x_new_gen)
-    
+    ea = EA(pop_size, cross_prob, mut_prob)
 
-def generate_init_pop(pop_size):
-    pass # return array of 195 bits
+    for i in range(num_gen):
+        if i % int(num_gen * 0.1) == 0:
+            print('generation: {}, best value: {:}'.format(i, max(f_gen)))
 
+        x_gen, f_fen = ea.step(x_gen, f_gen)
+
+
+# This function creates the initial population
+def generatePopulation(pop_size):
+    # type: (int) -> list[str]
+
+    generation = []  # initialize return variable
+    for i in range(pop_size):
+        # create a single gene
+        gene = ''
+        for i in range(GENE_SIZE):
+            # update single gene
+            gene = gene + str(randint(0, 1))
+        # check for errors before adding the gene to the gene pool
+        assert len(gene) == GENE_SIZE
+        generation.append(gene)
+
+    return generation
+
+
+# Return a list of fitness values for the generation
 def evaluate(x_gen):
-    pass # return array of fitness values
+    # type: (list[str]) -> list[int]
+
+    f_gen = []
+
+    for gene in x_gen:
+        board = Board()
+        f_gen.append(board.evaluateGene(gene))
+
+    return f_gen
+
+
+class Board:
+    def __init__(self):
+        self.x_axis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        self.y_axis = [1, 2, 3, 4, 5, 6, 7, 8]
+
+        self.cells = [(x, y) for x in self.x_axis
+                      for y in self.y_axis]  # get all cells of the chess board
+
+        self.positions = [x for x in range(0, 64)]  # iteration list
+
+        self.cells_and_pos = zip(self.positions,
+                                 sorted(self.cells, key=lambda x: x[1]))
+
+        self.current_pos = None  # saves the current position of the piece
+        self.visited_cells = []  # saves visited cells
+
+    # Return all legal moves for the knight figure based on current position
+    def getKnightMoves(self, position):
+        # type: (tuple(str, int)) -> list[tuple(str, int)]
+        x, y = position
+
+        # create all posible moves for knight travel trajectory
+        moves = list(product([chr(ord(x) - 1), chr(ord(x) + 1)],
+                             [y - 2, y + 2])) + \
+                list(product([chr(ord(x) - 2), chr(ord(x) + 2)],
+                             [y - 1, y + 1]))
+
+        # out of bounds check
+        moves = [(x, y) for (x, y) in moves if (x, y) in self.cells]
+
+        return moves
+
+    # Binary code to cell on board converter
+    def translateBinaryToCell(self, binary):
+        # type: (str) -> tuple(str, int)
+
+        # convert binary to decimal
+        decimal = 0
+        for digit in binary:
+            decimal = decimal * 2 + int(digit)
+
+        # match the resulting integer with location on chess board
+        ret = [cell for pos, cell in self.cells_and_pos if decimal == pos]
+
+        return ret[0]
+
+    # Splits the gene into a list of moves
+    def splitGene(self, gene):
+        # type: (str) -> list[str]
+
+        splitted_gene = []  # initialize return value
+        splitted_gene.append(gene[0:6])  # append the starting position
+
+        # slice the gene without starting position
+        gene_without_start = gene[6:-1]
+
+        moves = [gene_without_start[i:i + 3]
+                 for i in range(0, len(gene_without_start), 3)]
+
+        splitted_gene.extend(moves)
+
+        return splitted_gene
+
+    # Make a move from the current position and returns new location
+    def updatePosition(self, curr_pos, move):
+        # type: (tuple(str, int), tuple(str, int)) -> tuple(str, int)
+
+        if move == '000':
+            location = (chr(ord(curr_pos[0]) + 1), curr_pos[1] - 2)
+
+        elif move == '001':
+            location = (chr(ord(curr_pos[0]) + 2), curr_pos[1] - 1)
+
+        elif move == '010':
+            location = (chr(ord(curr_pos[0]) + 2), curr_pos[1] + 1)
+
+        elif move == '011':
+            location = (chr(ord(curr_pos[0]) + 1), curr_pos[1] + 2)
+
+        elif move == '100':
+            location = (chr(ord(curr_pos[0]) - 1), curr_pos[1] + 2)
+
+        elif move == '101':
+            location = (chr(ord(curr_pos[0]) - 2), curr_pos[1] + 1)
+
+        elif move == '110':
+            location = (chr(ord(curr_pos[0]) - 2), curr_pos[1] - 1)
+
+        elif move == '111':
+            location = (chr(ord(curr_pos[0]) - 1), curr_pos[1] - 2)
+
+        return location
+
+    # Returns fitness value
+    def evaluateGene(self, gene):
+        # type: (str) -> int
+
+        # check for errors before adding the gene to the gene pool
+        assert len(gene) == GENE_SIZE
+
+        fitness_value = 0
+
+        # split the gene and get initial location of the piece
+        splitted_gene = self.splitGene(gene)
+        self.current_pos = self.translateBinaryToCell(splitted_gene[0])
+        self.visited_cells.append(self.current_pos)
+
+        # iterate through the moves and update fitness value
+        for move in splitted_gene[1:-1]:
+            valid_moves = self.getKnightMoves(self.current_pos)
+            location = self.updatePosition(self.current_pos, move)
+
+            if location in valid_moves and location not in self.visited_cells:
+                fitness_value += 1
+
+            self.current_pos = location
+            self.visited_cells.append(location)
+
+        return fitness_value
+
+
+main(pop_size, num_gen, cross_prob, mut_prob)
--- module2.py
+++ module2.py
@@ -1,11 +1,40 @@
-def select_parents():
-    pass
+from random import randint
 
-def crossover():
-    pass
+GENE_SIZE = 195
 
-def mutation():
-    pass
+class EA(object):
+    def __init__(self, pop_size, cross_prob, mut_prob):
+        self.pop_size = pop_size
+        self.crossover_p = cross_prob
+        self.mutation_p = mut_prob
 
-def select_children():
-    pass
+    def parent_selection(self, x_old, f_old):
+        pass
+
+    # Apply random one-point crossover to get new individuals
+    def recombination(self, x_parents, f_parents):
+        # type: (list[str], list[int]) -> list[str]
+
+        # pick a random crossover point
+        recombination_point = randint(0, GENE_SIZE)
+        
+        flag=0
+        for i in range(GENE_SIZE):
+            if flag==0:
+                child.gene_pool[i] = self.gene_pool[i]
+                if i==crossover_point:
+                    flag = 1
+            elif flag==1:
+                child.gene_pool[i] = mate.gene_pool[i]
+        return child
+
+
+    def mutation(self, old_x_children, old_f_children):
+        pass
+
+
+    def survivor_selection(self, x_children, x_old, f_children, f_old):
+        pass
+
+    def step(self, x_old, f_old):
+        pass
